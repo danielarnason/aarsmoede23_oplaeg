a947142282b14c8742a2aa317c1986ddccaee943
Daníel Örn Árnason <danielarnason85@gmail.com>
2023-09-06 12:21:11 +0200

    DONE

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import Reveal from "reveal.js";
 import 'reveal.js/dist/reveal.css'
-import 'reveal.js/dist/theme/white.css'
+import 'reveal.js/dist/theme/serif.css'
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js'
+import 'reveal.js/plugin/highlight/zenburn.css'
 
-let deck = new Reveal()
+let deck = new Reveal({
+    hash: true,
+    plugins: [
+        RevealHighlight
+    ]
+})
 
 deck.initialize()
