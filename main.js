096cd325de97209a6a20a65a29da2391dcf65629
Daníel Örn Árnason <danielarnason85@gmail.com>
2023-09-06 13:49:21 +0200

    Nu med notes

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/serif.css'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js'
 import 'reveal.js/plugin/highlight/zenburn.css'
+import RevealNotes from 'reveal.js/plugin/notes/notes.js'
 
 let deck = new Reveal({
     hash: true,
     plugins: [
-        RevealHighlight
+        RevealHighlight,
+        RevealNotes
     ]
 })
 
