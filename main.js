9dc63c9001e96af0e88d929e06271d7fd7aece27
Daníel Örn Árnason <danielarnason85@gmail.com>
2023-09-06 15:08:07 +0200

    vite config og fireworks nu med

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import 'reveal.js/dist/theme/serif.css'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js'
 import 'reveal.js/plugin/highlight/zenburn.css'
 import RevealNotes from 'reveal.js/plugin/notes/notes.js'
+import { Fireworks } from 'fireworks-js'
 
 let deck = new Reveal({
     hash: true,
@@ -11,6 +12,18 @@ let deck = new Reveal({
         RevealHighlight,
         RevealNotes
     ]
+})
+
+deck.on('slidetransitionend', event => {
+    let curSlide = deck.getIndices()
+    console.log(curSlide)
+
+    if (curSlide.h == 8 && curSlide.v == 0) {
+        let container = document.querySelector('.slides')
+        let fireworks = new Fireworks(container)
+        fireworks.start()
+    }
+    
 })
 
 deck.initialize()
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: '/aarsmoede23_oplaeg/'
+})
