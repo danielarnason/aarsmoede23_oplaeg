380023d8edda78a89246a04614370e82af3d937d
Daníel Örn Árnason <danielarnason85@gmail.com>
2023-09-07 08:52:50 +0200

    Titel ændret

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js'
 import 'reveal.js/plugin/highlight/zenburn.css'
 import RevealNotes from 'reveal.js/plugin/notes/notes.js'
 import { Fireworks } from 'fireworks-js'
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 let deck = new Reveal({
     hash: true,
